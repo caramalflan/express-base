577d9d4d83bc38555325fb2cb4ff14d4674407c4
Alex Yoon <yoon512@gmail.com>
2020-11-07 12:14:50 -0500

    install sass; add sass script

--- views/layout.jsx
+++ views/layout.jsx
@@ -12,7 +12,10 @@ let Layout = (props) => {
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
           integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
-          crossorigin="anonymous" />
+          crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="/public/stylesheets/app.css" />
       </head>
       <body>
         {props.children}
